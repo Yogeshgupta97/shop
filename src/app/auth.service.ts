d244018f641a5dea64a4c732462ae549b0768d88
DESKTOP-YG\yogesh <yogesh.gupta@msn.com>
2018-12-13 20:44:37 +0530

    Authentication and Authorisation

---
+++ src/app/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
+import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/add-user';
+import { UserService } from './user.service';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
+
+@Injectable()
+export class AuthService {
+  user$: Observable<firebase.User>;
+
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute) { 
+    this.user$ = afAuth.authState;
+  }
+
+  login() {
+    let returnURL = this.route.snapshot.queryParamMap.get('returnURL') || '/';
+    localStorage.setItem('returnURL', returnURL);
+
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .switchMap(user => {
+        if (user) return this.userService.get(user.uid);
+
+        return Observable.of(null);
+      })
+  }
+}
