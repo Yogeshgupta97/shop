a4029ec703875e20cc99203f96ff727a0b089292
DESKTOP-YG\yogesh <yogesh.gupta@msn.com>
2018-12-15 18:18:05 +0530

    Product Management Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,15 @@ import { UserService } from './user.service';
 export class AppComponent {
   constructor(private userService: UserService, private auth: AuthService, router: Router) {
     auth.user$.subscribe(user => {
-      if (user) { 
-        userService.save(user);
-        
-        let returnUrl = localStorage.getItem('returnURL');
-        router.navigateByUrl(returnUrl);
-      }
+      if (!user) return;
+
+      userService.save(user);
+      
+      let returnUrl = localStorage.getItem('returnURL');
+      if (!returnUrl) return;
+
+      localStorage.removeItem('returnURL');
+      router.navigateByUrl(returnUrl);
     });
   }
 }
